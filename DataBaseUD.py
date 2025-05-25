8950188e57a3cbab205bd9dc458cdb415e26958c
Мамаева Ксения <ks.a.mamaeva@gmail.com>
2025-05-25 19:47:33 +0300

    Add files via upload

---
+++ DataBaseUD.py
@@ -0,0 +1,361 @@
+import ftplib
+import gzip
+import os
+import re
+import requests
+import scipy as sp
+
+import sqlite3 as sl
+import sqlalchemy as sa
+import pandas as pd
+
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column, relationship, Session
+from sqlalchemy import create_engine, DateTime, String, Float, Integer, func, ForeignKey, insert, select, text
+from sqlalchemy.dialects.postgresql import UUID as UUIDType
+from uuid import UUID, uuid4
+from typing import List
+from datetime import datetime
+
+import sensors
+
+
+
+# Path to downloaded files.
+dir_path: str = "data/downloads/"
+
+
+
+def load(year: int, month: int, day: int, agency: str, project: str, solution: str, content: str) -> str:
+    """
+    Download products from the IGN [1] FTP server by a specification.
+
+    Parameters
+    ----------
+    year:
+        Start time of the solution: year.
+    month:
+        Start time of the solution: month.
+    day:
+        Start time of the solution: day.
+    agency:
+        Analysis center providing the file (COD, EMR, ESA, GFZ, GRG, IGS, JAX, JPL, MIT, NGS,
+        SIO, SHA, WUH).
+    project:
+        Project within which the file was generated (MGX, OPS).
+    solution:
+        Solution type (FIN, RAP, ULT).
+    content:
+        Content type (ORB, SOL, ERP, ATT, CLK, OSB, GIM, TRO).
+
+    Returns
+    -------
+    path:
+        A local path to the downloaded file.
+
+    References
+    ----------
+    [1] Institut National de l'Information Géographique et Forestière (IGN) https://www.ign.fr/
+    """
+
+    # Get the time components.
+    t = sensors.Time.from_ymdhms(year, month, day, 0, 0, 0.0)
+    _, day_of_year, _ = t.yds
+    week = (t - sensors.Time.GPS_EPOCH) // sensors.Duration.WEEK
+
+    # Construct a pattern to search.
+    version = "[0-9]"
+    hour = "[0-9]{2}"
+    minute = "[0-9]{2}"
+    length = "[0-9]{2}[SMHDWLY]"
+    sampling = "[0-9]{2}[SMHDWLY]"
+    format_ = "\\S+"
+    pattern = re.compile(f"{agency}{version}{project}{solution}_" +
+                         f"{year:04d}{day_of_year:02d}{hour}{minute}_" +
+                         f"{length}_" +
+                         f"{sampling}_" +
+                         f"{content}." +
+                         f"{format_}")
+
+    # Create a directory for downloaded files.
+    os.makedirs(dir_path, exist_ok=True)
+
+    # Iterate over the existing files in the directory.
+    for filename in os.listdir(dir_path):
+        # Search the filename matched to the template.
+        match = pattern.match(filename)
+        if match is not None:
+            return dir_path + filename
+
+    # Connect to the FTP server.
+    with ftplib.FTP("igs.ign.fr") as ftp:
+        ftp.login("", "")
+
+        # Go to the actual directory.
+        ftp.cwd("pub/igs/products")
+        if project == "MGX":
+            ftp.cwd("mgex/")
+        ftp.cwd(f"{week:04d}/")
+
+        # Iterate over the files in the directory.
+        for zip_filename in ftp.nlst():
+            # Search the filename matched to the template.
+            match = pattern.match(zip_filename)
+            if match is not None:
+                # Download the file.
+                with open(zip_filename, "wb") as zip_file:
+                    ftp.retrbinary("RETR " + zip_filename, zip_file.write)
+
+                # Construct the path to the file.
+                nameparts = zip_filename.split(".")
+                path = dir_path + nameparts[0] + "." + nameparts[1]
+
+                # Unzip the file.
+                os.makedirs(dir_path, exist_ok=True)
+                with gzip.open(zip_filename, "rb") as zip_file:
+                    with open(path, "wb") as bin_file:
+                        bin_file.write(zip_file.read())
+
+                # Delete the archived file.
+                os.remove(zip_filename)
+
+                return path
+        else:
+            raise RuntimeError("cannot find file")
+
+"""
+Prepare for testing.
+"""
+
+# Select a date.
+year = 2024
+month = 11
+day = 1
+
+# Download the last ANTEX file.
+# path = load_antex()
+
+# Read the ANTEX file.
+# atx_data = sensors.gnss.ANTEXData().read(path)
+
+# Download an SP3 file.
+sp3_path = load(year, month, day, "COD", "MGX", "FIN", "ORB")
+
+# Download an ORBEX file.
+obx_path = load(year, month, day, "COD", "MGX", "FIN", "ATT")
+
+"""
+Test the SP3 processing tools.
+"""
+
+
+# Родительский класс таблиц
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+
+# Проверка данных на уникальность
+# Эта функция связывает словарь с сессией, которая хранит текущие "уникальные" ключи.
+def _unique(session, cls, hashfunc, queryfunc, constructor, arg, kw):
+    cache = session.info.get("_unique_cache", None)
+    if cache is None:
+        session.info['_unique_cache'] = cache = {}
+
+    key = (cls, hashfunc(*arg, **kw))
+    if key in cache:
+        return cache[key]
+    else:
+        with session.no_autoflush:
+            q = session.query(cls)
+            q = queryfunc(q, *arg, **kw)
+            obj = q.first()
+            if not obj:
+                obj = constructor(*arg, **kw)
+                session.add(obj)
+        cache[key] = obj
+        return obj
+
+
+# Проверка данных на уникальность
+# Методы для классов таблиц
+class UniqueMixin(object):
+    @classmethod
+    def unique_hash(cls, *arg, **kw):
+        raise NotImplementedError()
+
+    @classmethod
+    def unique_filter(cls, query, *arg, **kw):
+        raise NotImplementedError()
+
+    @classmethod
+    def as_unique(cls, session, *arg, **kw):
+        return _unique(
+            session,
+            cls,
+            cls.unique_hash,
+            cls.unique_filter,
+            cls,
+            arg, kw
+        )
+
+    @classmethod
+    async def async_as_unique(cls, async_session, *arg, **kw):
+        return await async_session.run_sync(cls.as_unique, *arg, **kw)
+
+
+class Satellates(UniqueMixin, Base):
+    __tablename__ = 'satellate'
+    __table_args__ = {'extend_existing': True}
+
+    id: Mapped[UUID] = mapped_column(
+        UUIDType(as_uuid=True),
+        primary_key=True,
+        default=uuid4,
+        index=True,
+        nullable=False)
+    prn: Mapped[str] = mapped_column(String(4), nullable=False)
+
+    epoch: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    agency_id: Mapped[int] = mapped_column(ForeignKey('agency.id'))
+    agency: Mapped['Agencies'] = relationship('Agencies', foreign_keys=[agency_id], back_populates='satellate')
+    system_id: Mapped[int] = mapped_column(ForeignKey('system.id'))
+    system: Mapped['Systems'] = relationship('Systems', foreign_keys=[system_id], back_populates='satellate')
+
+    x: Mapped[float] = mapped_column(Float, nullable=False)
+    y: Mapped[float] = mapped_column(Float, nullable=False)
+    z: Mapped[float] = mapped_column(Float, nullable=False)
+
+
+class Agencies(UniqueMixin, Base):
+    __tablename__ = 'agency'
+    __table_args__ = {'extend_existing': True}
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False, autoincrement=True, unique=True)
+    satellate: Mapped[list['Satellates']] = relationship('Satellates', back_populates='agency')
+    name: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+
+    @classmethod
+    def unique_hash(cls, name):
+        return name
+
+    @classmethod
+    def unique_filter(cls, query, name):
+        return query.filter(Agencies.name == name)
+
+
+class Systems(Base, UniqueMixin):
+    __tablename__ = 'system'
+    __table_args__ = {'extend_existing': True}
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False, autoincrement=True, unique=True)
+    satellate: Mapped[list['Sattellates']] = relationship('Satellates', back_populates='system')
+    name: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+
+    @classmethod
+    def unique_hash(cls, name):
+        return name
+
+    @classmethod
+    def unique_filter(cls, query, name):
+        return query.filter(Systems.name == name)
+
+
+
+# Read the SP3 file.
+sp3_data = sensors.gnss.read_sp3(sp3_path)
+
+# Get the agency creating the file.
+agency = sp3_data.agency
+
+# Iterate over the epochs.
+for epc_data in sp3_data.epochs:
+    # Get the date and time.
+    year, month, day, hour, minute, second = epc_data.t.ymdhms
+    cur_date = epc_data.t
+
+
+
+    # Iterate over the satellites.
+    for (sys, prn), sat_data in epc_data.satellites.items():
+        # Iterate over the records.
+        for rec_data in sat_data.records:
+            # If this is a position and clock record.
+            if isinstance(rec_data, sat_data.PositionAndClock):
+                # Get the satellite's coordinates.
+                x = rec_data.x
+                y = rec_data.y
+                z = rec_data.z
+
+                #print(sys.name, prn, x, y, z, cur_date)
+
+
+                path_to_db = 'D:/Проекты/hpc_engine/DateBaseSatellates.db'
+
+                if path_to_db:
+                    engine = create_engine(f'sqlite+pysqlite:///{path_to_db}', echo=False)
+                    connection = sl.connect('{path_to_db}')
+
+                Session = sessionmaker(bind=engine)
+                session = Session()
+
+                # Base.metadata.create_all(engine)
+
+
+                # test
+                _prn = prn
+                _agency = agency
+                _system = sys.name
+                _epoch = datetime(year, month, day, hour, minute)
+
+
+                _x = x
+                _y = y
+                _z = z
+
+                try:
+                    session.scalars(insert(Satellates).returning(Satellates),
+                                    [{'prn': _prn, 'agency': _agency, 'epoch': _epoch, 'system': _system,
+                                      'x': _x, 'y': _y, 'z': _z}])
+
+                    session.flush()
+                except:
+                    Agencies.as_unique(session, name=_agency)
+                    Systems.as_unique(session, name=_system)
+                    session.flush()
+                    session.scalars(insert(Satellates).returning(Satellates),
+                                    [{'prn': _prn, 'epoch': _epoch, 'system': _system, 'agency': _agency, 'x': _x,
+                                      'y': _y, 'z': _z,
+                                      'agency_id': session.scalars(select(Agencies).filter_by(name=_agency)).first().id,
+                                      'system_id': session.scalars(
+                                          select(Systems).filter_by(name=_system)).first().id}])
+
+                    session.flush()
+
+                session.commit()
+
+                # конец транзакции
+                session.rollback()
+
+
+
+
+                # Put to the database the next values: agency, year, month, day, hour, minute, second, sys, prn,
+                # x, y, z.
+
+""" Запросы в БД """
+# вывод в Датафрейм
+query = """
+SELECT ag.name as agency, epoch, sys.name as system, prn, x, y, z
+FROM satellate as sat
+JOIN agency as ag on sat.agency_id = ag.id
+JOIN system as sys on sat.system_id = sys.id
+WHERE epoch BETWEEN '2024-11-01 12:00:00' AND '2024-11-01 21:15:00' and sys.name = 'GPS' and prn = 14;
+    """
+satell = pd.read_sql(query, engine)
+#satell.set_index('id', inplace=True)
+satell
+
+#вывод result
+with session:
+    result = session.execute(text("SELECT * FROM agency"))
+    for row in result:
+        print(row)
